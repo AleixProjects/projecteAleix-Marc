6c85d19a83974cc875f1fe7c9bb4345f59879a98
Aleix <aleix.povedano@cirvianum.cat>
2024-10-22 20:47:47 +0200

    Template done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts}", "./app/*"],
   theme: {
     extend: {},
+    colors: {
+      beix: "#fefae0",
+      verdClar: "#606c38",
+      verdFosc: "#283618",
+      taronjaFosc: "#bc6c25",
+      taronjaClar: "#dda15e",
+    },
   },
   plugins: [],
-}
+};
