1b899ee7d62dd38bf86d8bdc299e55d3e003765e
mpenasgarrote <148444829+mpenasgarrote@users.noreply.github.com>
2024-11-04 11:23:06 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts}", "./app/*"],
+  theme: {
+    extend: {},
+    colors: {
+      beix: "#fefae0",
+      verdClar: "#606c38",
+      verdFosc: "#283618",
+      taronjaFosc: "#bc6c25",
+      taronjaClar: "#dda15e",
+      vermell: "#B00020",
+      blanc: "#FFFFFF"
+    },
+    fontFamily: {
+      ubuntu: "Ubuntu",
+    },
+  },
+  plugins: [],
+};
