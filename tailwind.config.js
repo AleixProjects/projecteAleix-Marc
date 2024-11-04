bf8f4e1084387bef8582306f5192ce53f5f18854
Marc Penas <marc.penas@cirvianum.cat>
2024-11-04 12:47:08 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts}", "./app/*"],
-  theme: {
-    extend: {},
-    colors: {
-      beix: "#fefae0",
-      verdClar: "#606c38",
-      verdFosc: "#283618",
-      taronjaFosc: "#bc6c25",
-      taronjaClar: "#dda15e",
-      vermell: "#B00020",
-      blanc: "#FFFFFF"
-    },
-    fontFamily: {
-      ubuntu: "Ubuntu",
-    },
-  },
-  plugins: [],
-};
+export default {
+	content: ['./**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}', './app/*'],
+	theme: {
+		extend: {},
+		colors: {
+			beix: '#fefae0',
+			verdClar: '#606c38',
+			verdFosc: '#283618',
+			taronjaFosc: '#bc6c25',
+			taronjaClar: '#dda15e',
+			vermell: '#B00020',
+			blanc: '#FFFFFF',
+		},
+		fontFamily: {
+			ubuntu: 'Ubuntu',
+		},
+	},
+	plugins: [],
+}
