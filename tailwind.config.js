f66b32327569d78e04bcff605c5dd7e8ba7287d5
Aleix <aleix.povedano@cirvianum.cat>
2024-11-04 01:04:15 +0100

    MainMenu done + responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
       verdFosc: "#283618",
       taronjaFosc: "#bc6c25",
       taronjaClar: "#dda15e",
-      vermell: "#B00020"
+      vermell: "#B00020",
+      blanc: "#FFFFFF"
     },
     fontFamily: {
       ubuntu: "Ubuntu",
